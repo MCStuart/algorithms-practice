4ce1b10699f4d58a3d94dc3335d1d9a621a96359
Stuart McKay <stuart51994@gmail.com>
2020-04-23 14:38:31 -0700

    add all test cases

--- src/EndsWith.js
+++ src/EndsWith.js
@@ -1,14 +1,3 @@
-/*
-Check if a string (first argument, str) ends with the given target string (second argument, target)
-NOTE: This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
-confirmEnding("Bastian", "n") should return true.
-confirmEnding("Congratulation", "on") should return true.
-confirmEnding("Connor", "n") should return false.
-confirmEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification") should return false.
-confirmEnding("He has to give me a new name", "name") should return true.
-confirmEnding("Open sesame", "same") should return true.
-*/
-
 const endsWith = () => {
 
 }
--- test/EndsWith.test.js
+++ test/EndsWith.test.js
@@ -6,13 +6,9 @@ NOTE: This challenge can be solved with the .endsWith() method, which was introd
 
 test('Check if a string (first argument, str) ends with the given target string (second argument, target)', () => {
   expect(endsWith("Bastian", "n")).toBe(true);
+  expect(endsWith("Congratulation", "on")).toBe(true);
+  expect(endsWith("Connor", "n")).toBe(false);
+  expect(endsWith("Walking on water and developing software from a specification are easy if both are frozen", "specification")).toBe(false);
+  expect(endsWith("He has to give me a new name", "name")).toBe(true);
+  expect(endsWith("Open sesame", "same")).toBe(true);
 })
-
-// Check if a string (first argument, str) ends with the given target string (second argument, target)
-// NOTE: This challenge can be solved with the .endsWith() method, which was introduced in ES2015. But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead.
-// confirmEnding("Bastian", "n") should return true.
-// confirmEnding("Congratulation", "on") should return true.
-// confirmEnding("Connor", "n") should return false.
-// confirmEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification") should return false.
-// confirmEnding("He has to give me a new name", "name") should return true.
-// confirmEnding("Open sesame", "same") should return true.
