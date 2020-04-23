1cfdc9125e998bdc93cbb8feaacff481bf6cd406
Stuart McKay <stuart51994@gmail.com>
2020-04-23 14:38:22 -0700

    add all test cases

--- test/ContainsAllInString.test.js
+++ test/ContainsAllInString.test.js
@@ -2,4 +2,6 @@ const containsAllInString = require('../src/ContainsAllInString');
 
 test('Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array', () => {
   expect(containsAllInString(["hello", "Hello"])).toBe(true);
-})
+  expect(containsAllInString(["hello", "hey"])).toBe(false);
+  expect(containsAllInString(["Alien", "line"])).toBe(true);
+});
