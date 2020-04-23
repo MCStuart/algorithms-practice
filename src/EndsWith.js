900a03a3af53f428fcbc6e7dbc0c18bcbbc2d935
Stuart McKay <stuart51994@gmail.com>
2020-04-23 15:31:00 -0700

    write function body for endsWith() to pass all test cases

--- src/EndsWith.js
+++ src/EndsWith.js
@@ -1,5 +1,10 @@
-const endsWith = () => {
-
+const endsWith = (str, target) => {
+  let charactersFromEnd = str.length - target.length;
+  let EndCharArray = str.substring(charactersFromEnd);
+  if (EndCharArray == target) {
+    return true
+  }
+  return false
 }
 
 module.exports = endsWith;
