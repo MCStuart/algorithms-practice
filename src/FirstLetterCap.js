53c81ac8ea8687b33c171e10a7e89c4930b4e64b
Stuart McKay <stuart51994@gmail.com>
2020-04-23 14:21:38 -0700

    set up test for firstLetterCap()

--- src/FirstLetterCap.js
+++ src/FirstLetterCap.js
@@ -6,3 +6,9 @@ titleCase("I'm a little tea pot") should return I'm A Little Tea Pot.
 titleCase("sHoRt AnD sToUt") should return Short And Stout.
 titleCase("HERE IS MY HANDLE HERE IS MY SPOUT") should return Here Is My Handle Here Is My Spout.
 */
+
+const firstLetterCap = () => {
+
+}
+
+module.exports = firstLetterCap;
