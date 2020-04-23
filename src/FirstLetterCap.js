e76353be99c58c69f698ebdb2f2d47929c8aad69
Stuart McKay <stuart51994@gmail.com>
2020-04-23 15:04:44 -0700

    write function body for firstLetterCap() to pass all test cases

--- src/FirstLetterCap.js
+++ src/FirstLetterCap.js
@@ -1,5 +1,13 @@
-const firstLetterCap = () => {
-
+const firstLetterCap = (sentence) => {
+  let allLowerCaseLetters = sentence.toLowerCase();
+  // let onlyAlphaChars = allLowerCaseLetters.replace(/[^a-zA-Z]/g, '');
+  let sentenceArray = allLowerCaseLetters.split(' ');
+  let capitalizedChars = [];
+  for (let i=0; i<sentenceArray.length; i++) {
+    capitalizedChars.push(sentenceArray[i].charAt(0).toUpperCase() + sentenceArray[i].slice(1));
+  }
+  let recombinedSentence = capitalizedChars.join(' ');
+  return recombinedSentence;
 }
 
 module.exports = firstLetterCap;
--- test/FirstLetterCap.test.js
+++ test/FirstLetterCap.test.js
@@ -4,4 +4,4 @@ test('Return the provided string with the first letter of each word capitalized.
   expect(firstLetterCap("I'm a little tea pot")).toBe("I'm A Little Tea Pot");
   expect(firstLetterCap("sHoRt AnD sToUt")).toBe("Short And Stout");
   expect(firstLetterCap("HERE IS MY HANDLE HERE IS MY SPOUT")).toBe("Here Is My Handle Here Is My Spout");
-})
+});
