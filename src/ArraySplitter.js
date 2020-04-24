c38e29afe50d4410fef59800f37c7fb5c06a5e63
Stuart McKay <stuart51994@gmail.com>
2020-04-23 17:27:21 -0700

    write function body for arraySplitter() to pass all test cases

--- src/ArraySplitter.js
+++ src/ArraySplitter.js
@@ -1,14 +1,11 @@
-/*
-Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
-chunkArrayInGroups(["a", "b", "c", "d"], 2) should return [["a", "b"], ["c", "d"]].
-chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3) should return [[0, 1, 2], [3, 4, 5]].
-chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2) should return [[0, 1], [2, 3], [4, 5]].
-chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4) should return [[0, 1, 2, 3], [4, 5]].
-chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3) should return [[0, 1, 2], [3, 4, 5], [6]].
-*/
-
-const arraySplitter = () => {
-
-}
-
+const arraySplitter = (arrayToSplitDefinition) => {
+  let arrayToSplit = arrayToSplitDefinition[0];
+  let definedLengthOfNewArrayChunks = arrayToSplitDefinition[1];
+  let twoDimensionalArray = [];
+  while (arrayToSplit.length > 0) {
+    twoDimensionalArray.push(arrayToSplit.slice(0, definedLengthOfNewArrayChunks));
+    arrayToSplit.splice(0, definedLengthOfNewArrayChunks);
+  };
+  return twoDimensionalArray;
+};
 module.exports = arraySplitter;
--- test/ArraySplitter.test.js
+++ test/ArraySplitter.test.js
@@ -1,9 +1,9 @@
 const arraySplitter = require('../src/ArraySplitter');
 
 test('splits an array (first argument) into groups the length of the second argument and returns them as a two-dimensional array', () => {
-  expect(arraySplitter([["a", "b", "c", "d"], 2])).toBe([["a", "b"], ["c", "d"]]);
-  expect(arraySplitter([[0, 1, 2, 3, 4, 5], 3])).toBe([[0, 1, 2], [3, 4, 5]]);
-  expect(arraySplitter([[0, 1, 2, 3, 4, 5], 2])).toBe([[0, 1], [2, 3], [4, 5]]);
-  expect(arraySplitter([[0, 1, 2, 3, 4, 5], 4])).toBe([[0, 1, 2, 3], [4, 5]]);
-  expect(arraySplitter([[0, 1, 2, 3, 4, 5, 6], 3])).toBe([[0, 1, 2], [3, 4, 5], [6]]);
+  expect(arraySplitter([["a", "b", "c", "d"], 2])).toMatchObject([["a", "b"], ["c", "d"]]);
+  expect(arraySplitter([[0, 1, 2, 3, 4, 5], 3])).toMatchObject([[0, 1, 2], [3, 4, 5]]);
+  expect(arraySplitter([[0, 1, 2, 3, 4, 5], 2])).toMatchObject([[0, 1], [2, 3], [4, 5]]);
+  expect(arraySplitter([[0, 1, 2, 3, 4, 5], 4])).toMatchObject([[0, 1, 2, 3], [4, 5]]);
+  expect(arraySplitter([[0, 1, 2, 3, 4, 5, 6], 3])).toMatchObject([[0, 1, 2], [3, 4, 5], [6]]);
 })
