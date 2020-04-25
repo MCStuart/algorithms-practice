f2d9935c6608883655350e871aa56c87f34d123d
Stuart McKay <stuart51994@gmail.com>
2020-04-25 16:04:55 -0700

    add two new test cases

--- test/ContainsAllInString.test.js
+++ test/ContainsAllInString.test.js
@@ -4,4 +4,6 @@ test('Return true if the string in the first element of the array contains all o
   expect(containsAllInString(["hello", "Hello"])).toBe(true);
   expect(containsAllInString(["hello", "hey"])).toBe(false);
   expect(containsAllInString(["Alien", "line"])).toBe(true);
+  expect(containsAllInString(["return undefined", "refined"])).toBe(true);
+  expect(containsAllInString(["this should work", "unexpected"])).toBe(false);
 });
