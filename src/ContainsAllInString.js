5905cb59fc905abef0115c0a7c87b2b7e7dd6509
Stuart McKay <stuart51994@gmail.com>
2020-04-25 16:04:46 -0700

    write function body for containsAllInString() to pass all test cases

--- src/ContainsAllInString.js
+++ src/ContainsAllInString.js
@@ -1,5 +1,13 @@
-const containsAllInString = () => {
-
-}
-
+const containsAllInString = (array) => {
+  let firstString = array[0].toLowerCase();
+  let isContained = array[1].toLowerCase();
+  let firstStringToArray = firstString.split('');
+  let isContainedInArray = isContained.split('');
+  for (let i = 0; i < isContainedInArray.length; i++) {
+    if (firstStringToArray.includes(isContainedInArray[i]) != true) {
+      return false;
+    };
+  };
+  return true;
+};
 module.exports = containsAllInString;
