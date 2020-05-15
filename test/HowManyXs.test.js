44576d9ce407207fe5778b339603995a3ff9b9bb
Stuart McKay <stuart51994@gmail.com>
2020-05-15 14:22:13 -0700

    create more expect statements for small value testing

--- test/HowManyXs.test.js
+++ test/HowManyXs.test.js
@@ -20,5 +20,9 @@ expect.extend({
 });
 
 test("Count how many occurances of 'X'(10) there are in the range of Roman Numerals [1,1000)", () => {
-  expect(xCounter()).toBeWithinRange(1000, 2000);
+  expect(xCounter(9999)).toBeWithinRange(15000, 17000);
+  expect(xCounter(9)).toBe(1);
+  expect(xCounter(10)).toBe(2);
+  expect(xCounter(15)).toBe(7);
+  expect(xCounter(100)).toBe(150);
 })
